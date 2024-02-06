549c9ffdc7a0b39117615bfc502e169802488065
Shivanandmetri <shivanand1998metri@gmail.com>
2024-02-06 23:36:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Our App is Deployed..
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
