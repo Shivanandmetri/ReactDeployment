274d4b6665a3191c2355ca441e1079004c9da3a8
Shivanandmetri <shivanand1998metri@gmail.com>
2024-02-07 09:37:30 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changes Happened 
+          Shivanand Swamy Raju
         </p>
       </header>
     </div>
